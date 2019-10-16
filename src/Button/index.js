c3be1a041b3e619d9791ed079ec366533f0ac609
Per Baltzar <per.baltzar@gmail.com>
2019-10-16 15:45:09 +0200

    added comments

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './style.scss';
 
+// The { text } is deconstructed from props. 
+// Instead of writing (props) and then props.text you can deconstruct props into { text, onClick, color }
+// or whatever you chose to pass through, when you add the component in App.js you just add the 
+// props you want to follow. <Button text="SEND" color="blue" onClick={handleClick} /> etc.
 const Button = ({ text }) => {
   return <button>{text}</button>;
 };
