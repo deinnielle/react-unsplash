567cc57aace50b7eddd5b69c7687f6f473b506ed
Mark <markeddd19@gmail.com>
2019-10-16 14:53:04 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 	}
 
 	handleSubmit(event) {
-    fetch('https://api.unsplash.com/search/photos/?client_id=30086014b47d3da23e1a9b2fa85837f0ca041c5ce34d4bfab637c45988c5ce08&page=1&per_page=10&orientation=portrait&query=' + this.state.value)
+    fetch(`https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_CLIENTID}&page=1&per_page=10&orientation=portrait&query=` + this.state.value)
     .then(res => res.json())
     .then(
       (result) => {
@@ -38,6 +38,7 @@ class App extends Component {
 	}
 
 	render() {
+    console.log(process.env);
 		return (
       <section>
         <nav>
