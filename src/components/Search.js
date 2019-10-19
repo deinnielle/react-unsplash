cf96b7c8c336a3696b9a53ddde173f13d36d00a9
Mark <markeddd19@gmail.com>
2019-10-19 15:55:58 +0200

    updated

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,19 +3,21 @@ import React, { useState, useEffect } from 'react';
 const SearchBar = () => {
 	const [value, setValue] = useState('random');
   const [images, setImages] = useState([]);
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(0);
   
   useEffect(() => {
-    console.log(count);
     fetch(`https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_CLIENTID}&page=${count}&per_page=10&query=${value}`)
     .then(res => res.json())
     .then((result) => {
       setImages(result.results);
     })
+
   },[value])
 
   const nextPage = event => {
-    setCount(prevCount => prevCount + 1)
+ 
+    setCount(count + 1);
+    
     console.log(count);
     
     fetch(`https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_CLIENTID}&page=${count}&per_page=10&query=${value}`)
@@ -27,7 +29,8 @@ const SearchBar = () => {
   }
   
   const prevPage = event => {
-    setCount(prevCount => prevCount -1)
+
+    setCount(count - 1);
     console.log(count);
 
     fetch(`https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_CLIENTID}&page=${count}&per_page=10&query=${value}`)
@@ -42,18 +45,14 @@ const SearchBar = () => {
     setValue(event.target.value);
   }
 
-  const handleSubmit = event => {
-    fetch(`https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_CLIENTID}&page=1&per_page=10&query=${value}`)
-    .then(res => res.json())
-    .then((result) => {
-      setImages(result.results);
-    })
-    event.preventDefault();
-  }
-
 	return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={prevPage}>PREV</button>
+        <button onClick={nextPage}>MORE</button>
+      </div>
+      <form>
         <input type="text" value={value} onChange={handleChange} />
         <button>SEND</button>
       </form>
@@ -63,11 +62,8 @@ const SearchBar = () => {
           <div className="text">{image.description}</div>
         </div>
       ))}
-      <div>
-        <button onClick={prevPage}>PREV</button>
-        <button onClick={nextPage}>MORE</button>
-      </div>
     </div>
 	)
 }
+
 export default SearchBar;
