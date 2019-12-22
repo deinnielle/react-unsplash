55524be27d71c08422d9f1163aaef089674dc0d5
Mark <markeddd19@gmail.com>
2019-12-22 21:53:50 +0100

    updated

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,20 +1,19 @@
-import React, {useContext} from 'react';
-import {Link} from 'react-router-dom';
-import {AppContext} from './Context';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Items = (props) => {
+const Items = props => {
   return (
     <div>
-    {props.data.map(image => (
-      <Link to={`/item/${image.id}`} key={image.id}>
-        <div className="grid-item">
-          <img src={image.urls.small} alt={image.description} />
-          <div className="text">{image.description}</div>
-        </div>
-      </Link>
-    ))}
+      {props.data.map(image => (
+        <Link to={`/item/${image.id}`} key={image.id}>
+          <div className="grid-item">
+            <img src={image.urls.small} alt={image.description} />
+            <div className="text">{image.description}</div>
+          </div>
+        </Link>
+      ))}
     </div>
   );
-}
+};
 
 export default Items;
