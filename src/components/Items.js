b5a31e7b1b81f7d5e5db3525ad3ace4ee7260dc9
Mark <markeddd19@gmail.com>
2019-12-30 13:44:20 +0100

    updated description

--- src/components/Items.js
+++ src/components/Items.js
@@ -4,11 +4,20 @@ import { Link } from "react-router-dom";
 const Items = props => {
   return (
     <div>
+      {console.log(props.data[2])}
       {props.data.map(image => (
         <Link to={`/item/${image.id}`} key={image.id}>
           <div className="grid-item">
-            <img src={image.urls.small} alt={image.description} />
-            <p className="text">{image.description}</p>
+            <img src={image.urls.small} alt={image.alt_description} />
+            {image.alt_description ? (
+              <p>
+                {image.alt_description.length > 20
+                  ? image.alt_description.slice(0, 20)
+                  : image.alt_description}
+              </p>
+            ) : (
+              <p>No description</p>
+            )}
           </div>
         </Link>
       ))}
